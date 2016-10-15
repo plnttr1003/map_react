5c030514276e5dba37a7708142a64ea9df31b9d3
plnttr1003 <top@nrdn.ru>
2016-10-15 21:00:50 +0300

    l5 || manage react component state with setstate

--- App.js
+++ App.js
@@ -1,7 +1,21 @@
 import React from 'react';
 class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			txt: 'this is the state txt'
+		};
+	}
+	update(e) {
+		this.setState({txt: e.target.value})
+	}
 	render() {
-		return <div>Hello</div>
+		return (
+			<div>
+				<input type="text" onChange={this.update.bind(this)}/>
+				<h1>{this.state.txt}</h1>
+			</div>
+		)
 	}
 }
 
