c8d978608f6a5b10eab861a0e48c9afedf9c3584
plnttr1003 <top@nrdn.ru>
2016-10-15 20:28:52 +0300

    l4 || set properties on react component

--- App.js
+++ App.js
@@ -1,17 +1,23 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 class App extends React.Component {
 	render() {
-		/*return <div>
-				<h1>Hello World</h1>
-				<b></b>
-			</div>*/
-		return (
-			<div>
-				<h1>Hello World</h1>
-				<b></b>
-			</div>
-		);
+		let txt = this.props.txt
+		let txt1 = this.props.txt
+		return <h1>По умолчанию: {txt1} Текст: {txt} Число: {this.props.cat}</h1>
 	}
 }
 
-export default App
+App.propTypes = {
+	txt: React.PropTypes.string,
+	cat: React.PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+	txt1: 'this is the default txt'
+}
+
+ReactDOM.render(
+	<App cat={5} txt="this is props text 123"/>,
+	document.getElementById('app')
+);
