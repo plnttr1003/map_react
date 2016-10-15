880a8af71fec89fb899beb9ddcc585ea0ecaffa8
plnttr1003 <top@nrdn.ru>
2016-10-15 20:03:04 +0300

    l3

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import React from 'react';
-/*class App extends React.Component {
+class App extends React.Component {
 	render() {
-		return <div>Hello World</div>
-	}
-	render() {
-		return React.createElement('h1', null, '1212')
+		/*return <div>
+				<h1>Hello World</h1>
+				<b></b>
+			</div>*/
+		return (
+			<div>
+				<h1>Hello World</h1>
+				<b></b>
+			</div>
+		);
 	}
 }
-*/
-const App = () => <div><h1>123</h1>232321 323<h2>23</h2></div>
+
 export default App
