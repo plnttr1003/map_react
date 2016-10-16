8d7807b623c5a5df6dcbf2b953e4adafb0340247
plnttr1003 <top@nrdn.ru>
2016-10-16 14:18:13 +0300

    l11 || Control React Component Updates When New Props Are Received

--- App.js
+++ App.js
@@ -1,15 +1,35 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 class App extends React.Component {
+	constructor() {
+	  super();
+	  this.update = this.update.bind(this);
+	  this.state = {increasing: false}
+	}
+	update() {
+		ReactDOM.render(
+			<App val={this.props.val+1} />,
+			document.getElementById('app')
+		);
+	}
+	componentWillReceiveProps(nextProps){
+		this.setState({increasing:nextProps.val > this.props.val})
+	}
+	shouldComponentUpdate(nextProps, nextState) {
+	  return nextProps.val % 5 === 0; // обновляется каждый пятый раз
+	}
 	render() {
-		return <Button>I <Heart/> React</Button>
+		console.log(this.state.increasing);
+		return (
+			<button onClick={this.update}>
+				{this.props.val}
+			</button>
+		)
+  }
+  componentDidUpdate(prevProps, prevState) {
+    console.log('prevProps', prevProps);
   }
 }
-class Button extends React.Component {
-	render() {
-		return <button>{this.props.children}</button>
-	}
-}
 
-const Heart = () => <span className="glyphicon glyphicon-heart">2</span>
-
+App.defaultProps = {val:0}
 export default App
